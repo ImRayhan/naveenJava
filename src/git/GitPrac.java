0ce4b8e986e40920a367b44c98a8465d4ce18861
ImRayhan <rayhanc@yahoo.com>
2024-07-24 16:11:52 -0400

    updated gitprac page

--- src/git/GitPrac.java
+++ src/git/GitPrac.java
@@ -1,5 +1,17 @@
 package git;
 
 public class GitPrac {
+	
+	/*
+	 * git branch = check how many branch in my sytem
+	 * git branch java = java here create new branch
+	 * git checkout java = swith to java branch
+	 * git status = check git current status
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("change the code as per git pull req");
+	}
 
 }
